edcbaf5ef4827480a4b65c7eeab7e18319dec437
Rudi Klassen <rudolfklassen2@gmail.com>
2017-09-21 19:55:38 +0200

    Delete incomplete test

--- server/src/test/java/de/budgetfreak/budgetfreakapplication/BudgetfreakapplicationApplicationTests.java
+++ server/src/test/java/de/budgetfreak/budgetfreakapplication/BudgetfreakapplicationApplicationTests.java
@@ -1,24 +1,14 @@
 package de.budgetfreak.budgetfreakapplication;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-
-@RunWith(SpringJUnit4ClassRunner.class)
+/*@RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
 						 DirtiesContextTestExecutionListener.class,
 						 TransactionalTestExecutionListener.class,
 						 DbUnitTestExecutionListener.class})
-@DatabaseSetup("classpath:testdatabase.xml")
+@DatabaseSetup("classpath:testdatabase.xml")*/
 public class BudgetfreakapplicationApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 	}
 
