01a84f43b29777721f05e33857a272e162b1a570
Lukas Korten <lukas.korten@adesso.de>
2017-10-12 21:19:00 +0200

    Create top and second level routing (#42)
    * top level routing defined; empty components for accounts, login and budgeting
    
    * second level routing defined
    
    * test fix
    
    * remove initial e2e test
    
    * change routes
    
    removed prefix `/user` for all child routes

---
+++ web/src/app/user/accounts/accounts.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-accounts',
+  templateUrl: './accounts.component.html'
+})
+export class AccountsComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
