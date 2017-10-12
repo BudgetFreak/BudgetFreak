7a03175ed508dbd7ee817ccbcd59e47a3f12c2a8
Lukas Korten <lukas.korten@adesso.de>
2017-10-12 20:43:06 +0200

    second level routing defined

--- web/src/app/app.routing.ts
+++ web/src/app/app.routing.ts
@@ -1,15 +1,11 @@
 import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
-import {BudgetingComponent} from "./user/budgeting/budgeting.component";
-import {AccountsComponent} from "./user/accounts/accounts.component";
-import {UserComponent} from "./user/user.component";
+import {USER_ROUTES, USER_ROUTING_COMPONENTS} from "./user/user.routing";
 
 export const APP_ROUTES: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'user', component: UserComponent},
-  {path: 'user/budgeting', component: BudgetingComponent},
-  {path: 'user/accounts', component: AccountsComponent}
+  ...USER_ROUTES
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
-export const ROUTING_COMPONENTS = [LoginComponent, BudgetingComponent, UserComponent, AccountsComponent];
+export const ROUTING_COMPONENTS = [LoginComponent, ...USER_ROUTING_COMPONENTS];
---
+++ web/src/app/user/user.routing.ts
@@ -0,0 +1,14 @@
+import {UserComponent} from "./user.component";
+import {Routes} from "@angular/router";
+import {BudgetingComponent} from "./budgeting/budgeting.component";
+import {AccountsComponent} from "./accounts/accounts.component";
+
+export const USER_ROUTES: Routes = [{
+  path: 'user', component: UserComponent,
+  children: [
+    {path: '', component: BudgetingComponent},
+    {path: 'accounts', component: AccountsComponent}
+  ]
+}];
+
+export const USER_ROUTING_COMPONENTS = [UserComponent, BudgetingComponent, AccountsComponent];
