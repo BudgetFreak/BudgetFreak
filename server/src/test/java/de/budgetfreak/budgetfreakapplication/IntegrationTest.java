2c429825ed87ee371a74c5ac82997a3a7243112d
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2017-12-14 19:53:48 +0100

    fix spring profile

--- server/src/test/java/de/budgetfreak/budgetfreakapplication/IntegrationTest.java
+++ server/src/test/java/de/budgetfreak/budgetfreakapplication/IntegrationTest.java
@@ -8,6 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = BudgetfreakapplicationApplication.class)
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 public abstract class IntegrationTest {
 }
