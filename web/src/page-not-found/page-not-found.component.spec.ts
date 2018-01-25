d19a28d81d6aafcd8750190cd1af01752ce1e3cc
Rudi Klassen <rudolfklassen2@gmail.com>
2018-01-25 22:03:49 +0100

    Deleted unnecessary page-not-found Component

--- web/src/page-not-found/page-not-found.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import {PageNotFoundComponent} from './page-not-found.component';
-
-describe('Component: PageNotFound', () => {
-  it('should create an instance', () => {
-    const component = new PageNotFoundComponent();
-    expect(component).toBeTruthy();
-  });
-});
