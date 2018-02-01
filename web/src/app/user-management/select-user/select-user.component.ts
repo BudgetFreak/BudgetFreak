39a25ef3149a0903e217cb98e63d7d0c6f477bb0
Lukas Korten <lukas.korten@adesso.de>
2018-02-01 19:17:03 +0100

    Budget selection (#56)

---
+++ web/src/app/user-management/select-user/select-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-user',
+  templateUrl: './select-user.component.html'
+})
+export class SelectUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
