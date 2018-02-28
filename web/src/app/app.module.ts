ff2392e27edd6cd43f771c624739aa225bc2e447
Rudi Klassen <rudolfklassen2@gmail.com>
2018-02-28 18:24:32 +0100

    fixed missing imports

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,6 +3,8 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {APP_ROUTING, ROUTING_COMPONENTS} from "./app.routing";
+import {UserListComponent} from "./user/user-list/user-list.component";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
