9c0f359cdca19fd887c4b947b090ac40382dd160
Rudi Klassen <6597008+RudiKlassen@users.noreply.github.com>
2018-02-28 18:31:15 +0100

    Angj4 to rest (#55)
    * First Proof of Concept
    * List users in Angular UI

---
+++ web/src/app/user/user.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from "@angular/core";
+import {User} from "./user";
+import {Http, Response} from "@angular/http";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {Observable} from "rxjs/Observable";
+
+@Injectable()
+export class UserService {
+
+  private apiUrl = 'http://localhost:8080/users';
+
+  constructor(private http: Http) {
+
+  }
+
+  findAll(): Observable<User[]> {
+    return this.http.get(this.apiUrl)
+      .map((response: Response) => response.json()._embedded.userResources)
+      .catch((err: any) => Observable.throw(err.json().error || 'Server error'));
+  }
+
+  findById(id: Number): Observable<User> {
+    //TODO rukl@all implement
+    return null;
+  }
+
+  createUser(user: User): Observable<User> {
+    //TODO rukl@all implement
+    return null;
+  }
+
+  deleteUser(user: User): Observable<User> {
+    //TODO rukl@all implement
+    return null;
+  }
+
+  updateUser(user: User): Observable<User> {
+    //TODO rukl@all implement
+    return null;
+  }
+}
