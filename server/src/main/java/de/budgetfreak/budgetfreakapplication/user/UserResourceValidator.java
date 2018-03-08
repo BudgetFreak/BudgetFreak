20bf70a49f49fffd963bd9d1211b8d01b9579645
Sebastian Herbermann <acetous@users.noreply.github.com>
2018-03-08 18:20:12 +0100

    Extend user controller (#59)
    * ignore rebel.xml files
    * Get a single UserResource with /users/{id}
    * Validate a UserResource on create
    * remove unused exception

---
+++ server/src/main/java/de/budgetfreak/budgetfreakapplication/user/UserResourceValidator.java
@@ -0,0 +1,24 @@
+package de.budgetfreak.budgetfreakapplication.user;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * Validator for {@link UserResource}.
+ */
+@Component
+public class UserResourceValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return UserResource.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "name.empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "currency", "name.empty");
+    }
+}
