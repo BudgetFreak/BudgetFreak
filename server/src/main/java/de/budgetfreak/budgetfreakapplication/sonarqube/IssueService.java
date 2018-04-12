a03bb4e8587d991b659c01e4cd0e65e443a028d7
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2018-04-12 16:39:14 +0200

    add more sonar issues

--- server/src/main/java/de/budgetfreak/budgetfreakapplication/sonarqube/IssueService.java
+++ server/src/main/java/de/budgetfreak/budgetfreakapplication/sonarqube/IssueService.java
@@ -1,14 +1,28 @@
 package de.budgetfreak.budgetfreakapplication.sonarqube;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 @Service
 public class IssueService {
+    private static final Logger log = LoggerFactory.getLogger(IssueService.class);
 
     public void issue() {
         boolean variable_with_naming_issue = true;
         if (!variable_with_naming_issue) {
             throw new RuntimeException("No one should do this");
+        }
+    }
+
+    public void moreIssues() {
+        try {
+            Files.createTempFile("testing", "tmp");
+        } catch (IOException e) {
+            log.error("Could not create temp file.");
         }
     }
 }
