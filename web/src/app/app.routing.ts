3fb7b1bc2cd8c17eb2982e397f8cb9c087ecf239
Sebastian Herbermann <acetous@users.noreply.github.com>
2018-04-12 21:01:32 +0200

    Refactor urls (#77)
    * `/` redirects to `/login`
    * `/budgeting` is now `/budget`

--- web/src/app/app.routing.ts
+++ web/src/app/app.routing.ts
@@ -1,13 +1,15 @@
 import {RouterModule, Routes} from "@angular/router";
 import {USER_ROUTES, USER_ROUTING_COMPONENTS} from "./user/user.routing";
 import {UserManagementComponent} from "./user-management/user-management.component";
-import {USER_MANAGEMENT_ROUTING_COMPONENTS, USER_MANAGEMENT_ROUTES} from "./user-management/user-management.routes";
+import {USER_MANAGEMENT_ROUTES, USER_MANAGEMENT_ROUTING_COMPONENTS} from "./user-management/user-management.routes";
 
 export const APP_ROUTES: Routes = [
-  {path: '', component: UserManagementComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   ...USER_MANAGEMENT_ROUTES,
   ...USER_ROUTES
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(
+  APP_ROUTES
+);
 export const ROUTING_COMPONENTS = [...USER_ROUTING_COMPONENTS, ...USER_MANAGEMENT_ROUTING_COMPONENTS];
