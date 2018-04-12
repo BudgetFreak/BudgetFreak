8b17ea08ba135b46ed07f7504eaea8fd9f76b149
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2018-04-12 17:28:27 +0200

    add issues

---
+++ server/src/main/java/de/budgetfreak/budgetfreakapplication/sonarqube/IssueService.java
@@ -0,0 +1,20 @@
+package de.budgetfreak.budgetfreakapplication.sonarqube;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Files;
+
+@Service
+public class IssueService {
+    private static final Logger my_logger = LoggerFactory.getLogger(IssueService.class);
+
+    public void issue() {
+        boolean variable_with_naming_issue = true;
+        if (!variable_with_naming_issue) {
+            throw new RuntimeException("No one should do this");
+        }
+    }
+
+}
