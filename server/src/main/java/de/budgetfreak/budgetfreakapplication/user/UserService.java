0cea4f70b1c02f729346aff9f266d345abc8ffba
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2018-06-02 11:16:44 +0200

    fix sonar issue

--- server/src/main/java/de/budgetfreak/budgetfreakapplication/user/UserService.java
+++ server/src/main/java/de/budgetfreak/budgetfreakapplication/user/UserService.java
@@ -5,7 +5,6 @@ import de.budgetfreak.budgetfreakapplication.user.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
